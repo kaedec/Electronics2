bd1aef0d3b4f7be1a98318c8bb6861f8ebff7841
kaedec <kaecharbonneau@gmail.com>
2018-01-04 22:22:12 -0500

    Testing stuff out w/ octave

---
+++ Homework00/Homework00_1.m
@@ -0,0 +1,24 @@
+%Katelyn Charbonneau
+%EE320 Homework 00 Question 1
+
+%Constant declarations
+
+clear all
+clc
+
+VDD=10;
+vo=5;
+CB=0.1*10^-6;
+R=15*10^3;
+RL=10*10^6;
+RG=10*10^6;
+Kn=270*10^-6;
+VTn=0.573;
+LAMDAn=0.0165;
+Kp=88*10^-6;
+VTp=-0.647;
+LAMBDAp=0.0219;
+
+
+
+%Part A
