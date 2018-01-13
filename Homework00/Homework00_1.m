1c4a20415d921d0eea5920592eb59edcf4726719
kaedec <kaecharbonneau@gmail.com>
2018-01-12 22:42:15 -0500

    did some work

--- Homework00/Homework00_1.m
+++ Homework00/Homework00_1.m
@@ -6,19 +6,25 @@
 clear all
 clc
 
-VDD=10;
-vo=5;
-CB=0.1*10^-6;
-R=15*10^3;
-RL=10*10^6;
-RG=10*10^6;
-Kn=270*10^-6;
-VTn=0.573;
-LAMDAn=0.0165;
-Kp=88*10^-6;
-VTp=-0.647;
-LAMBDAp=0.0219;
-
-
+VDD = 10; vo = 5;
+CB = 0.1e-6;
+R = 15e3; RL = 10e6; RG = 10e6;
+G = 1/R; GL = 1/RL; GG = 1/RG;
+Kn = 270e-6; VTn = 0.573; Lambdan = 0.0165;
+Kp = 88e-6; VTp = -0.647; Lambdap = 0.0219;
 
 %Part A
+Vov3 = VovQuad(R*Kp, 1, abs(VTp)-VDD);
+ID3 = Kp*Vov3^2;
+VG3 = ID3*R;
+VG2 = VG3;
+
+VSG2 = VDD-ID3*R;
+Vov2 = VSG2 - abs(VTp);
+ID2 = Kp*abs(Vov2)^2;
+
+ID1 = ID2;
+Vov1 = sqrt(ID1/Kn);
+VSG1 = Vov1+VTn;
+
+VGS3 = VG3-VDD
